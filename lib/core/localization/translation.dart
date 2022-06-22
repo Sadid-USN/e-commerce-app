8834f551114bdd06f8b89ac4fe2ff72afd3a0b2b
MYNAME <myname@MacBook-Pro-MYNAME.local>
2022-06-22 12:14:48 +0600

    added login page design

--- lib/core/localization/translation.dart
+++ lib/core/localization/translation.dart
@@ -3,7 +3,7 @@ import 'package:get/route_manager.dart';
 class MyTranslation extends Translations {
   @override
   Map<String, Map<String, String>> get keys => {
-        "en": {"title": "Welcome"},
+        "en": {"title": "Welcome", "2": ""},
         "uk": {"title": "Ласкаво просимо"},
         "ar": {"title": "أهلا وسهلا ومرحبا"},
         "ru": {"title": "Добро пожаловать"},
